79ed1b3bf022a59204e59228b577a2af86541831
Jamie Cameron <jcameron@webmin.com>
2008-03-13 23:38:13 +0000

    No need to force http-based logins for mobiles

--- postinstall.pl
+++ postinstall.pl
@@ -9,7 +9,7 @@ local %miniserv;
 &get_miniserv_config(\%miniserv);
 if (!defined($miniserv{'mobile_preroot'})) {
 	$miniserv{'mobile_preroot'} = "virtual-server-mobile";
-	$miniserv{'mobile_nosession'} = 1;
+	#$miniserv{'mobile_nosession'} = 1;
 	$miniserv{'mobile_prefixes'} = "m. mobile.";
 	&put_miniserv_config(\%miniserv);
 	$gconfig{'mobile_theme'} = "virtual-server-mobile";
