e62cb15542a10d2bd37a9361aca15691bc39b262
Jamie Cameron <jcameron@webmin.com>
2009-03-02 01:45:10 +0000

    Re-factor functions used by cgis, support new module

---
+++ virtual-server-mobile-lib.pl
@@ -0,0 +1,18 @@
+# Functions for mobile theme CGIs
+
+eval "use WebminCore;";
+if ($@) {
+	do '../web-lib.pl';
+	do '../ui-lib.pl';
+	}
+&init_config();
+%text = &load_language($current_theme);
+#&load_theme_library();
+
+sub theme_use_iui
+{
+return $ENV{'HTTP_USER_AGENT'} =~ /iPhone|iPod|Android/;
+}
+
+1;
+
